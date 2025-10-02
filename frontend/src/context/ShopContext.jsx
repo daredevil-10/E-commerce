40612a1db9cecbd11131529bdc416e09350071b6
Vipul <vipuly41@gmail.com>
2025-10-03 02:42:04 +0530

    Implemented Search property

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { ShopContext } from "./contextFile";
 
 const ShopContextProvider = (props) => {
   const currency = "₹";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return (
